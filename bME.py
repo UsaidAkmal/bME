bd8b5121be3e52cee2a9935fdb2ea8a24f69162c
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-07-14 08:57:01 +0700

    semuanya baik baik saja :)

--- bME.py
+++ bME.py
@@ -7,3 +7,20 @@ Calculator BMI (Body Mass Index)
                        
 ''')
 
+berat = float(input("Berapa berat badan kamu? (kg) "))
+tinggi = float(input("Berapa tinggi badan kamu? (cm)"))
+
+tinggi = tinggi / 100
+
+bmi = berat / (tinggi ** 2)
+
+print(f"\nBmi Kamu Adalah: {bmi: .2f}")
+
+if bmi < 18.5:
+    print("Kategori: Kurus")
+elif bmi < 25:
+    print("Kategori: Normal")
+elif bmi < 30:
+    print("Kategori: Gemuk")
+else:
+    print("Kategori: Obesitas")
